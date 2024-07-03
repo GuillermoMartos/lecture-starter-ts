608c338c6057d4e1298634ddc41cee966061cf44
Guillermo Martos <guille.l.martos@gmail.com>
2024-07-03 14:46:01 +0200

    - + feat add favorite movies logic

--- api/utils.ts
+++ api/utils.ts
@@ -1,6 +1,21 @@
 import { OverwriteAPIResponse } from './constants';
 import { MovieResultsResponse } from './types';
 
+function setLocalStoredFavoriteMovies(movieID: number) {
+    const localStoragedMovies = localStorage.getItem('favoriteMovies');
+    if (localStoragedMovies) {
+        const storedMoviesArray: number[] = JSON.parse(localStoragedMovies);
+        if (storedMoviesArray.includes(movieID)) {
+            const filteredMoviesArray = storedMoviesArray.filter((num) => num !== movieID);
+            localStorage.setItem('favoriteMovies', JSON.stringify(filteredMoviesArray));
+            return;
+        }
+        localStorage.setItem('favoriteMovies', JSON.stringify([...storedMoviesArray, movieID]));
+    } else {
+        localStorage.setItem('favoriteMovies', JSON.stringify([movieID]));
+    }
+}
+
 function getRequiredElement<T extends Element>(parent: Element | Document, selector: string): T {
     const element = parent.querySelector<T>(selector);
     if (!element) {
@@ -9,20 +24,43 @@ function getRequiredElement<T extends Element>(parent: Element | Document, selec
     return element;
 }
 
+function hydrateFavoriteHeartIcon(cardParent: Element, movieFavoriteHeart: HTMLImageElement, movieId: number) {
+    const localStoragedMovies = localStorage.getItem('favoriteMovies');
+    if (localStoragedMovies && JSON.parse(localStoragedMovies).includes(movieId)) {
+        movieFavoriteHeart.setAttribute('fill', 'red');
+    }
+    movieFavoriteHeart.addEventListener('click', () => {
+        const isAlredyLiked = movieFavoriteHeart.getAttribute('fill') === 'red';
+        if (!isAlredyLiked) {
+            movieFavoriteHeart.setAttribute('fill', 'red');
+        } else {
+            movieFavoriteHeart.setAttribute('fill', '#ff000078');
+        }
+
+        const dataId = cardParent.getAttribute('data-id');
+        if (dataId) {
+            setLocalStoredFavoriteMovies(+dataId);
+        }
+    });
+}
+
 export function refreshDOMData(data: MovieResultsResponse[]) {
     const moviesContainer = document.querySelectorAll('.card');
     let extraRandomMovieIndex = 0;
     moviesContainer.forEach((card, index) => {
-        const { release_date: releaseDate, overview, poster_path: posterPath } = data[index];
+        const { release_date: releaseDate, overview, poster_path: posterPath, id } = data[index];
+        card.setAttribute('data-id', `${id}`);
         const movieImg = getRequiredElement<HTMLImageElement>(card, 'img');
         const movieDescription = getRequiredElement<HTMLElement>(card, '.card-text');
         const movieReleased = getRequiredElement<HTMLElement>(card, '.text-muted');
+        const movieFavoriteHeart = getRequiredElement<HTMLImageElement>(card, 'svg');
 
         movieImg.src = posterPath
             ? `https://image.tmdb.org/t/p/original/${posterPath}`
             : OverwriteAPIResponse.NO_POSTER_AVAILABLE;
         movieDescription.innerHTML = overview === '' ? OverwriteAPIResponse.NO_DESCRPTION_MESSAGGE : overview;
         movieReleased.innerHTML = releaseDate;
+        hydrateFavoriteHeartIcon(card, movieFavoriteHeart, id);
         extraRandomMovieIndex = index + 1;
     });
     const randomMovieCard = getRequiredElement<HTMLElement>(document, '#random-movie');
